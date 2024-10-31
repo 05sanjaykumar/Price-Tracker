ae7a0589d368b7a4c153d319ce5d980b6a710952
SanjayKumar <0508sanjaykumar@gmail.com>
2024-10-31 16:08:30 +0530

    Login added

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+  const [formData, setFormData] = useState({email:'',password:''})
+  const [error, setError] = useState()
+  const navigate = useNavigate()
+
+  const handleChange = (e)=>{
+    setFormData({...formData,[e.target.name]:e.target.value})
+  }
+
+  const handleSubmit = async(e)=>{
+    e.preventDefault()
+    try {
+      const response = await axios.post('/login',formData)
+      localStorage.setItem('token',response.data.token)
+      navigate('/dashboard')
+    } catch (error) {
+      setError(err.response?.data?.message || 'Login failed');
+    }
+  }
+
   return (
-    <div>Login</div>
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input type='email' name='email' placeholder='Email' value={formData.email} onChange={handleChange} required></input>
+        <input type="password" name='password' placeholder='Password' value={formData.password} onChange={handleChange} required/>
+        <button type='submit'>Login</button>
+      </form>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+    </div>
   )
 }
 
