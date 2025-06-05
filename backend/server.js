171cf30319c6a25a04fed013652d1c90c5684508
SanjayKumar <0508sanjaykumar@gmail.com>
2025-06-05 10:04:11 +0530

    refactor: Changed backend

--- backend/server.js
+++ backend/server.js
@@ -2,37 +2,22 @@ const express = require('express')
 const cors = require('cors')
 const connectDB = require('./config/db')
 const authRoutes = require('./Routes/authRoutes');
-const googleRoutes=require('./Routes/googleApiRoutes');
-const getImage = require('./Routes/getImageRoute')
-const getWebsites = require('./Routes/priceVariations')
+const getPrice = require('./Routes/getPrices')
+const itemRoutes=require('./Routes/itemRoutes')
 
 connectDB()
 
 const app = express()
 
-// const generateToken = require('./utils/generateToken');
+
 const PORT = 3000
 app.use(cors())
 app.use(express.json())
 
-app.use('/api/auth',googleRoutes);
 
-//changes made
-/* ----------------------------------- */
-//Routes/searchRoutes file created
-const searchRoutes = require('./Routes/searchRoutes')
-app.use('/search',searchRoutes)
-
-/* ----------------------------------- */
 app.use('/auth', authRoutes);
-const itemRoutes=require('./Routes/itemRoutes')
 app.use("/item",itemRoutes);
-
-app.use('/getImage',getImage);
-app.use('/checkWebsites',getWebsites);
-app.get('/',(req,res)=>{
-    res.send('Hello world')
-})
+app.use('/price',getPrice)
 
 
 app.listen(PORT,(req,res)=>{
