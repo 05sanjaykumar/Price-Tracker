958f54b6b7281c0a1d8dee860df54c76a42f47b4
SanjayKumar <0508sanjaykumar@gmail.com>
2025-06-07 17:21:10 +0530

    feat: Completed Backend and now needed for testing actually

--- __backend__/Routes/AuthRoutes.js
+++ __backend__/Routes/AuthRoutes.js
@@ -2,9 +2,10 @@
 
 const express = require('express');
 const router = express.Router();
+const {SignInUser, LogInUser} = require('../Controllers/UserController')
 
-router.post('/signin');
-router.post('/login');
+router.post('/signin',SignInUser);
+router.post('/login', LogInUser);
 
 
 
